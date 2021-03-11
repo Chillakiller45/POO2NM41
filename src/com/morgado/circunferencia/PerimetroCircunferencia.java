e29ecd759eaf6dc8236e8573d2ad5fb231e8528b
Miguel Angel Morgado Huerta <miguelmorgado678@gmail.com>
2021-03-11 13:22:44 -0600

    Perimetro Circunferencia 1

--- src/com/morgado/circunferencia/PerimetroCircunferencia.java
+++ src/com/morgado/circunferencia/PerimetroCircunferencia.java
@@ -2,9 +2,14 @@ package com.morgado.circunferencia;
 
 public class PerimetroCircunferencia {
 
-    public void devolverPerimetro() {
+    public static void main(String[] args) {
+        devolverPerimetro();
+    }
+
+    public static void devolverPerimetro() {
+        final double PI = Math.PI;
         double radio = 25.0;
-        double perimetro = 2.0*Math.PI*radio;
+        double perimetro = 2.0 * PI * radio;
 
         System.out.println("El perimetro de la circunferencia de radio ");
         System.out.println(radio);
